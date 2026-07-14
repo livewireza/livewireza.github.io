85e4754799c390fe63e8236c4631f3bd762324d3
Livewireza <92034037+livewireza@users.noreply.github.com>
2026-07-14 12:18:15 +0200

    Create app.js
    app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+async function loadNews() {
+
+    const response = await fetch("../data/news.json");
+    const news = await response.json();
+
+    const container = document.getElementById("news");
+
+    container.innerHTML = "";
+
+    news.forEach(article => {
+
+        const card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+            <h2>${article.title}</h2>
+            <small>${article.source}</small>
+            <p>${new Date(article.published).toLocaleString()}</p>
+            <a href="${article.link}" target="_blank">
+                Read article
+            </a>
+        `;
+
+        container.appendChild(card);
+
+    });
+
+}
+
+loadNews();
